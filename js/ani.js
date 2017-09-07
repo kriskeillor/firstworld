5ceb978c5b5754cf71a209e772a287d73b5d4934
Kris with a K <kris.keillor@gmail.com>
2017-09-06 19:41:28 -0700

    Separate UI and animation code into two files

---
+++ js/ani.js
@@ -0,0 +1,85 @@
+//	p for planet 
+var p = {
+	r:		100,		// planet radius, in pixels 
+	tilt:	105,		// pole tilt, counterclowckwise from x-axis 
+	angle:	0,			// relative to star, in degrees 
+	
+	origin:	{ 
+		x:	500, 
+		y:	200,
+	},
+};
+
+//	initialize pole positions, as we can't reference r/tilt in the p initializer 
+p.north = {
+	x:	-Math.cos(toRad(p.tilt)) * p.r + p.origin.x,
+	y:	-Math.sin(toRad(p.tilt)) * p.r + p.origin.y,
+},
+p.south = {
+	x:	Math.cos(toRad(p.tilt)) * p.r + p.origin.x,
+	y:	Math.sin(toRad(p.tilt)) * p.r + p.origin.y,
+}
+
+function toRad(deg) {
+	return (deg / 180) * Math.PI;
+}
+
+function wrapDegrees(deg) {
+	while (deg < 0) 
+		deg += 360;
+	return deg % 360;
+}
+
+function writeArc(xRad, yRad, rotation, sweepFlag, endX, endY) {
+	// radius 0 glitches arc rendering, so draw a line instead 
+	if (Math.floor(xRad) == 0 || Math.floor(yRad) == 0 || 
+		Math.ceil(xRad) == 0 || Math.ceil(yRad) == 0)
+		return "L " + endX + ' ' + endY;
+	else
+		return "A " + xRad + ' ' + yRad + ' ' + rotation + " 0 " + sweepFlag + ' ' + endX + ' ' + endY;
+}
+
+function writeBand(id, leading, trailing, xray) {
+	leading = wrapDegrees(leading);
+	trailing = wrapDegrees(trailing);
+	
+	var leadRadius;
+	if (!xray && (leading > 180)) // || leading < 0))	// deprecated by new wrapDegrees
+		leadRadius = -p.r;
+	else
+		leadRadius = Math.cos(toRad(leading)) * p.r;
+	
+	var trailRadius;
+	if (!xray && (trailing > 180)) // || trailing < 0))	// deprecated by new wrapDegrees
+		trailRadius = -p.r;
+	else
+		trailRadius	= Math.cos(toRad(trailing)) * p.r;
+	
+	if (!xray) {
+		if (trailing < 180 && leading > 180)
+			leadRadius = p.r;
+		if (leading > 180 && trailing < 180)
+			leadRadius = p.r;
+	}
+	
+	var path;
+	if (!xray && (leading == 180 && trailing == 0))
+		path = "";
+	else {
+		var leadSweep = 0;
+		if (leadRadius > 0)
+			leadSweep = 1;
+		
+		var trailSweep = 0;
+		if (trailRadius < 0)
+			trailSweep = 1;
+		
+		var move = "M " + p.north.x + ' ' + p.north.y;
+		var leadArc = writeArc(leadRadius, p.r, p.tilt-90, leadSweep, p.south.x, p.south.y);
+		var trailArc = writeArc(trailRadius, p.r, p.tilt-90, trailSweep, p.north.x, p.north.y);
+		
+		path = move + ' ' + leadArc + ' ' + trailArc;
+	}
+	
+	document.getElementById(id).setAttribute('d', path);
+}
--- js/ui.js
+++ js/ui.js
@@ -1,79 +1,3 @@
-// BG code here
-increment = 1.0;
-angle = 0;
-tilt = 105;
-r = 100;
-
-northX = -Math.cos(toRad(tilt)) * r + 500;
-northY = -Math.sin(toRad(tilt)) * r + 200;
-
-southX = Math.cos(toRad(tilt)) * r + 500;
-southY = Math.sin(toRad(tilt)) * r + 200;
-
-function toRad(deg) {
-	return (deg / 180) * Math.PI;
-}
-
-function wrapDegrees(deg) {
-	while (deg < 0) 
-		deg += 360;
-	return deg % 360;
-}
-
-function writeArc(xRad, yRad, rotation, sweepFlag, endX, endY) {
-	// radius 0 glitches arc rendering, so draw a line instead 
-	if (Math.floor(xRad) == 0 || Math.floor(yRad) == 0 || 
-		Math.ceil(xRad) == 0 || Math.ceil(yRad) == 0)
-		return "L " + endX + ' ' + endY;
-	else
-		return "A " + xRad + ' ' + yRad + ' ' + rotation + " 0 " + sweepFlag + ' ' + endX + ' ' + endY;
-}
-
-function writeBand(id, leading, trailing, xray) {
-	leading = wrapDegrees(leading);
-	trailing = wrapDegrees(trailing);
-	
-	var leadRadius;
-	if (!xray && (leading > 180)) // || leading < 0))	// deprecated by new wrapDegrees
-		leadRadius = -r;
-	else
-		leadRadius = Math.cos(toRad(leading)) * r;
-	
-	var trailRadius;
-	if (!xray && (trailing > 180)) // || trailing < 0))	// deprecated by new wrapDegrees
-		trailRadius = -r;
-	else
-		trailRadius	= Math.cos(toRad(trailing)) * r;
-	
-	if (!xray) {
-		if (trailing < 180 && leading > 180)
-			leadRadius = r;
-		if (leading > 180 && trailing < 180)
-			leadRadius = r;
-	}
-	
-	var path;
-	if (!xray && (leading == 180 && trailing == 0))
-		path = "";
-	else {
-		var leadSweep = 0;
-		if (leadRadius > 0)
-			leadSweep = 1;
-		
-		var trailSweep = 0;
-		if (trailRadius < 0)
-			trailSweep = 1;
-		
-		var move = "M " + northX + ' ' + northY;
-		var leadArc = writeArc(leadRadius, r, tilt-90, leadSweep, southX, southY);
-		var trailArc = writeArc(trailRadius, r, tilt-90, trailSweep, northX, northY);
-		
-		path = move + ' ' + leadArc + ' ' + trailArc;
-	}
-	
-	document.getElementById(id).setAttribute('d', path);
-}
-
 function load() {
 	minNavPane("cap");
 	minNavPane("runner");
@@ -88,13 +12,14 @@ function load() {
 		//var lead = wrapDegrees(angle+90);
 		//var trail = wrapDegrees(angle-90);
 		
-		writeBand('dawn', angle+90, angle-90, false);
+		writeBand('dawn', p.angle+90, p.angle-90, false);
 		//writeBand('day', angle+90, angle-90, false);
 		//writeBand('dusk', angle-90, angle-96, false);
 		//writeBand('arc', angle - 5, angle + 5, false);
 		
-		angle += increment;
-		angle %= 360;
+		//	!! 
+		p.angle += increment;
+		p.angle %= 360;
 	}, 20);
 }
 
@@ -102,10 +27,10 @@ function load() {
 // star
 altitude = -6;
 increment = 0.1;
- 
+
 // surface
 lux = 0;
- 
+
 var opening = {
 	1: "thick fog obscuring the riverbank.",
 	2: "reeds and cattails swaying in the mist.",
@@ -124,7 +49,7 @@ var opening = {
 	15: "better stop while there's daylight.",
 	len: 15,
 	count: 0,
-
+	
 	nextMsg: '<a onClick="expoNext();" class="clicker">keep going</a> | ',
 	stopMsg: '<a onClick="expoStop();" class="clicker">stop here</a>'
 };
