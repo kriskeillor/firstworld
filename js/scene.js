66bfb47ab52605cfd6525f0b411ffb7414c49258
Kris with a K <kris.keillor@gmail.com>
2017-09-11 22:10:50 -0700

    Add cooldowns to tasks and juice resources

--- js/scene.js
+++ js/scene.js
@@ -1,74 +1,74 @@
 var res = {
-	canvas:	0,
-	panels:	0,
-	keys:	0,
+	list: [ 'beans', 'polyester', 'powder' ],
+	discovered: [],
+	flash: { },		// transparency of background warning color upon insufficient resources 
+	
+	//natural 
+	beans:		0,
+	
+	//artificial
+	polyester:	0,
+	powder:		0,
 };
 
 var tasks = {
 	active: [],
 	
-	'castOff': {
-		msg:	"cast off",
+	'window': {
+		// animate: fade-in background gradient and planet 
+		msg:	"open window",
 		cost:	0,
-		time:	250,
+		life:	25,
+		cool:	0,
 		power:	0,
-		unlocks: [ 'rollOut', 'openPanels' ],
-		gain: {
-			canvas:	3,
-		}
+		unlocks: [ 'coffee', 'instruments' ],
+		gain:	0,
+		redo:	false,
 	},
 	
-	'takeBag': {
-		msg:	"take bag",
-		cost: 	0, 
-		time: 	150, 
-		power:	0,
-		unlocks:0,
-		gain: { 
-			canvas:	1,
-			keys:	1, 
-		}
-	},
-	
-	'rollOut': {
-		msg:	"roll out",
+	'instruments': {
+		msg:	"check instruments",
 		cost:	0, 
-		time:	500, 
-		power:	0,
-		unlocks: [ 'kickstart' ]
+		life:	150,
+		cool:	0,
+		power:	0,						// should this cost power? probably 
+		unlocks: 0,
+		gain:	0,
+		redo:	false,
 	},
 	
-	'openPanels': {
-		msg:	"open panels",
-		cost:	0,
-		time:	250,
+	'coffee': {
+		msg:	"brew coffee",
+		cost: { beans: 1 },
+		life:	300,
+		cool:	3000,
 		power:	0,
 		unlocks: 0,
-		gain: {
-			panels:	1,
-		}
+		gain:	0,
+		redo:	true,
 	},
 	
-	'kickstart': {
-		msg:	"kickstart",
-		cost: {
-			keys:	1,
-		},
-		time:	25,
-		power:	1,
-		unlocks: [ 'wheelsUp' ],
+	'gather': {
+		msg:	"gather supplies",
+		cost:	0,
+		life:	150,					// should be a function depending on needed resources 
+		cool:	5000,					// should also be a function 
+		power:	0,
+		unlocks: 0,
+		gain: { beans: 1 },
+		redo:	true,
 	},
-	
-	'wheelsUp': {
-		msg:	"end demo",
-		time:	0,
-	}
 };
+
+var state = {
+	foundResources:	false,
+	foundLogs:		false,	// actually, the logs button can just be appended whenever the first is found, so this isn't needed 
+}
 
 function initScene() {
 	minNavPane("log");
 	maxNavPane("ui");
+	minNavPane('res');
 	
-	addTask('castOff');
-	addTask('takeBag');
+	addTask('window');
 }
--- js/ui.js
+++ js/ui.js
@@ -13,12 +13,6 @@ function minNavPane(pane) {
 	document.getElementById(pane).style.display = "none";
 }
 
-function fillBar(barId) {
-	//if (test < 100)
-	//	test += 1;
-	//document.getElementById("bar" + id).style.width = test + '%';
-}
-
 function openLog() {
 	maxNavPane("log");
 	smallNavPane("ui");
@@ -36,7 +30,23 @@ function addTask(id) {
 }
 
 function startTask(id) {
-	tasks[id].life = tasks[id].time;
+	if (tasks.active.indexOf(id) != -1)
+		return;
+	
+	for (i = 0; i < res.list.length; i++) {
+		var resName = res.list[i];
+		
+		if (tasks[id].cost[resName] > res[resName])
+		{
+			flashRes(resName);
+			
+			if (res.discovered.indexOf(resName) == -1)
+				discoverRes(resName);
+			return;
+		}
+	}
+	
+	tasks[id].timer = tasks[id].life;
 	tasks.active.push(id);
 }
 
@@ -44,29 +54,83 @@ function updateTasks() {
 	finished = [];
 	
 	for (i = 0; i < tasks.active.length; i++) {
-		id = tasks.active[i];
-		tasks[id].life -= 1;
-		console.log("task " + id + " has life " + tasks[id].life);
-		if (tasks[id].life <= 0) 
+		var id = tasks.active[i];
+		tasks[id].timer -= 1;
+		
+		if (tasks[id].timer <= 0)
 			finished.push(id);
-		per = tasks[id].life / tasks[id].time;
-		console.log("width of " + id + " is " + per + "%");
-		document.getElementById("bar" + id).style.width = (100 - per * 100) + '%';
+		
+		// animation 
+		var per;
+		if (!tasks[id].cooling)
+			per = 100 - (tasks[id].timer / tasks[id].life) * 100;
+		else
+			per = (tasks[id].timer / tasks[id].cool) * 100;
+		document.getElementById("bar" + id).style.width = per + '%';
 	}
 	
 	for (i = 0; i < finished.length; i++) {
 		finishTask(finished[i]);
 	}
+	
+	updateFlashes();
 }
 
 function finishTask(id) {
-	document.getElementById("bar" + id).outerHTML = "";
-	
 	for (i = 0; i < tasks[id].unlocks.length; i++) { 
-		addTask(tasks[id].unlocks[i]);
+		if (tasks.active.indexOf(tasks[id].unlocks[i]) == -1)
+			addTask(tasks[id].unlocks[i]);
 	}
 	
-	index = tasks.active.indexOf(id);
-	tasks.active.splice(index, 1);
-	delete tasks[id];
+	if (tasks[id].cool > 0) {
+		if (!tasks[id].cooling) {
+			tasks[id].cooling = true;
+			tasks[id].timer = tasks[id].cool;
+		}
+		else {
+			tasks[id].cooling = false;
+			index = tasks.active.indexOf(id);
+			tasks.active.splice(index, 1);
+		}
+		
+		return;
+	}
+	else {
+		document.getElementById("bar" + id).outerHTML = "";
+		
+		index = tasks.active.indexOf(id);
+		tasks.active.splice(index, 1);
+		delete tasks[id];	
+	}
+}
+
+function discoverRes(resName) {
+	var resCon = document.getElementById("resCon");
+	if (resCon.innerHTML == "")
+		smallNavPane("res");
+	
+	res.discovered.push(resName);
+	resCon.innerHTML += "<tr class='resCounter' id='" + resName + "'><td>" + resName + "</td><td class='vital' id='" + resName + "Count'>" + 0 +"</td></tr>";
+}
+
+function flashRes(resName) {
+	// dawn color = fcb54d
+	// yourNumber.toString(16)
+	
+	res.flash[resName] = 1.0;
+}
+
+function spendRes(resName) {
+	// decrement counter in res and html 
+}
+
+function updateFlashes() {
+	var nodes = document.getElementsByClassName('resCounter');
+	for (i = 0; i < nodes.length; i++) {
+		var resName = nodes[i].id;
+		var bg = "rgba(252, 181, 77, " + res.flash[resName] + ")";			//"#" + res.flash[resName].toString(16);
+		console.log(bg);
+		nodes[i].style.backgroundColor = bg;
+		res.flash[resName] *= 0.9;
+	}
 }
